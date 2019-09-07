ef9156686d19f297d3e621bdb608b1776c577ed7
Rein Chau <rein.chau@firstidea.vn>
2019-09-07 11:17:42 +0700

    Removed git-commit.bat

--- git-publish.bat
+++
@@ -1,9 +0,0 @@
-git add -A
-git commit -m "Sync version with other packages; Published 2.3.0-rc1;"
-git checkout master
-git merge development
-git tag 2.3.0-rc1
-git checkout development
-git merge master
-git push --all
-git push --tags
